400fe4e50e58a626024e9059b3bdd0ba9bf28bbf
Amine Sadik <asadik@e1r5p7.1337.ma>
2022-10-29 12:43:11 +0100

    malloc fix and other

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: asadik <asadik@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 10:22:47 by asadik            #+#    #+#             */
-/*   Updated: 2022/10/22 00:10:43 by asadik           ###   ########.fr       */
+/*   Updated: 2022/10/29 12:36:05 by asadik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (start >= ft_strlen(s))
 		return (ft_strdup(""));
 	if (ft_strlen(s) < len)
-	{
-		sub = malloc(ft_strlen(s) + 1);
-		len = ft_strlen(s);
-	}
-	else
-		sub = malloc(len + 1);
+		len = ft_strlen(s) - start;
+	sub = malloc(len + 1);
 	if (!sub)
 		return (NULL);
 	while (i < len)
 	{
-		*(sub + i++) = *(s++ + start);
+		sub[i++] = *(s++ + start);
 	}
-	*(sub + i) = '\0';
+	sub[i] = '\0';
 	return (sub);
 }
